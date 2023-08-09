4445c190589b0c1b3d43d69e02e61ff804e142c9
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-08-09 14:29:11 +0300

    Completed Assignment

--- src/parks/howManyParks.js
+++ src/parks/howManyParks.js
@@ -1,5 +1,9 @@
+import { wildlife, elevation } from "../parks/RockyMountain";
+
 function howManyParks() {
   console.log("42 parks!");
+  wildlife();
+  elevation();
 }
 
 export default howManyParks;
